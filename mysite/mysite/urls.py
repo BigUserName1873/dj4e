49d2a79f8a2d7edc830c192eae5aaf2d88643420
bigusername <cantfindaname37>
2024-11-22 14:24:01 -0500

    please god work

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
     path('autos/', include('autos.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),    
     re_path(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
         name='site_path'
